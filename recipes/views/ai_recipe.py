7b99a548da790d821ca9d41649e728a73d9f3877
Mohammad Raza <mohammad.5.khan@kcl.ac.uk>
2025-11-23 19:46:08 +0000

    Create AI recipe view and the template for it. ALso create an env file which contains the API key. Also installed some packages

--- recipes/views/ai_recipe.py
+++ recipes/views/ai_recipe.py
@@ -2,35 +2,87 @@ from django.shortcuts import render
 import openai, os
 from openai import OpenAI
 from dotenv import load_dotenv
+from django.http.response import StreamingHttpResponse
 
 
 load_dotenv()
-OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 
 
-openai.api_key = OPEN_API_KEY
+#recipes = []
 
 
-
-
-#client = OpenAI(api_key=OPEN_API_KEY)
+client = OpenAI(api_key= os.getenv("OPEN_API_KEY"))
 
 def chatbot(request):
-    chatbot_response = None
-    if (request.method == 'POST'):
-        openai.api_key = OPEN_API_KEY
-        user_input = request.POST.get("user_input")
-        
+   chatbot_response = None
+   
+   recipes = request.session.get("recipes",[])
 
-        response = openai.completions.create(
-            engine = 'text-davinci-003',
-            prompt=user_input,
-            max_tokens=256, 
-            stop = ".",
-            temperature= 0.5,
+   if request.method == 'POST':
+        if "submit" in request.POST:
             
+          user_input = request.POST.get("user_input")
 
-        )
 
-    return render(request,"AI_recipe.html",{"response" : chatbot_response})
+          examplePrompt = f""" 
+          Ingredients: Chicken and Rice
+          Steps
+          1. Cook rice
+          2. Add Chicken
 
+          Ingredients: Eggs and Cheese
+          Steps
+          1. Beat Eggs
+          2. Add Cheese
+
+          Ingredients: {user_input} 
+          Recipe Title:
+          """
+
+
+
+
+
+          response = client.chat.completions.create(
+               model = "gpt-4o",
+               messages = [{"role": "system", "content" : "You are a helpful chef, your job is to "
+               "give a recipe name and give the  user brief instructions on how to make it"
+               "make it line by line. If it is invalid then say it's invalid"
+               "each instruction that you write should be on a new line"
+               "Put each instruction on a separate line pls."
+               "you're being too harsh with the invalid instructions"
+               "don't say invalid to every thing that can't be made"
+               "if they missed out some instructions then perhaps add some of your own"
+               "but obv don't add too many or make it into some crazy dish"
+               "then at the end you could briefly say, I have added these ingredients ..."
+               "but only do that if they only put in like a couple items and you literally"
+               "can't make anything with those"
+
+
+               "so in summary they layout be, recipe name \n "
+               "step 1 \n"
+               "step 2 \n"
+               "until all of the steps are done"
+               },
+
+
+               {"role":"user", "content":examplePrompt}])
+
+          chatbot_response = response.choices[0].message.content
+          instructions = chatbot_response.split("\n")
+          recipes.append(instructions)
+          request.session["recipes"] = recipes
+
+
+        if "clear_history" in request.POST:
+           request.session["recipes"] = []
+           recipes = []
+            
+     
+          
+       
+
+       # request.session = {"recipes" : chatbot_response}
+       
+        
+   return render(request,"AI_recipe.html",{"recipes": recipes})
