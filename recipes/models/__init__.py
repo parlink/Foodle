35239af231d6cd55016ab224d3e422acaced9263
Parlin Kaur (k24066668) <parlin.kaur@kcl.ac.uk>
2025-11-30 13:24:53 +0000

    Added feed models

--- recipes/models/__init__.py
+++ recipes/models/__init__.py
@@ -4,3 +4,7 @@ from .profile import *
 from .fasting_session import *
 from .water_intake import *
 from .meal import *
+from .comment import *
+from .like import *
+from .post import *
+from .tag import *
