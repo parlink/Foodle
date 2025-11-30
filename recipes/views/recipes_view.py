dfa1f052749319144baf22feeb3c930cfdca5aec
Daniel Mani <danieldmani@icloud.com>
2025-11-30 23:40:42 +0000

    Recipe_View bug fix

--- recipes/views/recipes_view.py
+++ recipes/views/recipes_view.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required
+#@login_required
 def recipes(request):
-    return render(request, 'recipes.html')
+    context = {
+        'recipes': []  
+    }
+    return render(request, 'recipes.html', context)
+
