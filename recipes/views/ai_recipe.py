ea45afe798a5d3f59dadba898d0d5dc1aba1b685
Mohammad Raza <mohammad.5.khan@kcl.ac.uk>
2025-12-09 13:36:03 +0000

    Edited the AI recipe view, added input handling.

--- recipes/views/ai_recipe.py
+++ recipes/views/ai_recipe.py
@@ -30,83 +30,83 @@ def chatbot(request):
 
 
 
-          examplePrompt = f"""     
-          Ingredients: Chicken and Rice
-          Steps
-          1. Cook rice
-          2. Add Chicken
+               examplePrompt = f"""     
+               Ingredients: Chicken and Rice
+               Steps
+               1. Cook rice
+               2. Add Chicken
 
-          Ingredients: Eggs and Cheese
-          Steps
-          1. Beat Eggs
-          2. Add Cheese
+               Ingredients: Eggs and Cheese
+               Steps
+               1. Beat Eggs
+               2. Add Cheese
 
-          Ingredients: {user_input} 
-          Recipe Title:
-          """
-
-
+               Ingredients: {user_input} 
+               Recipe Title:
+               """
 
 
-#This will create the submit prompt
-          response = client.chat.completions.create(
-               model = "gpt-4o",
-               messages = [{"role": "system",
-                             "content" : """You are a helpful chef, your job is to 
-               give a recipe name and give the  user brief instructions on how to make it
-               make it line by line. If it is invalid then say it's invalid
-               each instruction that you write should be on a new line
-               Put each instruction on a separate line
-               If there is not one valid ingredient, then reply with something like
-               this is an invalid ingredient etc etc, and DONT make a recipe for it
-               don't say invalid to every thing that can't be made
-               if they missed out some instructions then add some of your own
-               but don't add too many or make it into a crazy dish
-               then at the end briefly say, I have added these ingredients ...
-               but only do that if they only put a couple items and you literally
-               can't make anything with those
-
-               If an ingredient is not appropriate with the rest of the ingredients then do not 
-               include it in the recipe since it will not match.
-
-               Mention all of the ingredients needed right after the final step
-
-               Also add the nutrition info at the end very briefly though.
-               So like Calories: ___, Carbs: ___ etc. Put them all on one line
 
 
-               so in summary they layout be, recipe name \n 
-               step 1 \n
-               step 2 \n
-               until all of the steps are done """
-               },
-               {"role":"user", "content":examplePrompt}])
+               #This will create the submit prompt
+               response = client.chat.completions.create(
+                    model = "gpt-4o",
+                    messages = [{"role": "system",
+                              "content" : """You are a helpful chef, your job is to 
+                    give a recipe name and give the  user brief instructions on how to make it
+                    make it line by line. If it is invalid then say it's invalid
+                    each instruction that you write should be on a new line
+                    Put each instruction on a separate line
+                    If there is not one valid ingredient, then reply with something like
+                    this is an invalid ingredient etc etc, and DONT make a recipe for it
+                    don't say invalid to every thing that can't be made
+                    if they missed out some instructions then add some of your own
+                    but don't add too many or make it into a crazy dish
+                    then at the end briefly say, I have added these ingredients ...
+                    but only do that if they only put a couple items and you literally
+                    can't make anything with those
 
-          chatbot_response = response.choices[0].message.content
-          instructions = chatbot_response.split("\n")
+                    If an ingredient is not appropriate with the rest of the ingredients then do not 
+                    include it in the recipe since it will not match.
+
+                    Mention all of the ingredients needed right after the final step
+
+                    Also add the nutrition info at the end very briefly though.
+                    So like Calories: ___, Carbs: ___ etc. Put them all on one line
 
 
-          if instructions[0].lower().startswith("this is an invalid"):
-             recipes.append({"type":"invalid", "text":chatbot_response})
-             request.session["recipes"] = recipes
-             return redirect('ai_recipe')
+                    so in summary they layout be, recipe name \n 
+                    step 1 \n
+                    step 2 \n
+                    until all of the steps are done """
+                    },
+                    {"role":"user", "content":examplePrompt}])
+
+               chatbot_response = response.choices[0].message.content
+               instructions = chatbot_response.split("\n")
+
+
+               if "invalid" in chatbot_response.lower():
+                    recipes.append({"type":"invalid", "text":chatbot_response})
+                    request.session["recipes"] = recipes
+                    return redirect('ai_recipe')
           
 
 
-          nutrition_index = None
-          for i,line in enumerate(instructions):
-             if "Nutrition"in line or "Calories" in line:
-               nutrition_index = i
-               break
+               nutrition_index = None
+               for i,line in enumerate(instructions):
+                    if "Nutrition"in line or "Calories" in line:
+                         nutrition_index = i
+                         break
              
-          if nutrition_index is not None:
-               nutrition_block= " ".join(instructions[nutrition_index:])
-               instructions = instructions[:nutrition_index] + [nutrition_block]
+               if nutrition_index is not None:
+                    nutrition_block= " ".join(instructions[nutrition_index:])
+                    instructions = instructions[:nutrition_index] + [nutrition_block]
 
 
-          recipes.append({"type":"recipe","steps":instructions})
-          request.session["recipes"] = recipes
-          return redirect("ai_recipe")
+               recipes.append({"type":"recipe","steps":instructions})
+               request.session["recipes"] = recipes
+               return redirect("ai_recipe")
         
 
 
@@ -125,16 +125,19 @@ def chatbot(request):
           method = "\n".join(recipe_to_save[1:-1])
           nutrition = recipe_to_save[-1]
 
-          if Recipe.objects.filter(title=title).exists():
+          if Recipe.objects.filter(name=title).exists():
              return redirect("ai_recipe")
           
           placeholder = "images/ai_default_recipes.jpg"
 
           Recipe.objects.create(
-             title = title,
-             description = method + "\n\n" + nutrition,
+             name = title,
+             method = method + "\n\n" + nutrition,
              ingredients = request.session.get("last_ingredients", "AI ingredients"),
-             image = placeholder
+             total_time = "N/A",
+             servings = 1,
+             difficulty = "Easy",
+             average_rating = 0
 
 
           )
