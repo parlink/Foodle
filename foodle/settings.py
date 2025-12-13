aa80b972cb9915a3602dce771b49cfed947d1f6e
Ozan Kaya <k24029907@kcl.ac.uk>
2025-12-13 22:28:28 +0000

    Fix custom.css issue

--- foodle/settings.py
+++ foodle/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'assets/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'assets/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
